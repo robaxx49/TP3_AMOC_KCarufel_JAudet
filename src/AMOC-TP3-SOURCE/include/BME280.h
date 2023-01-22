32c474739e1be9811c5e4da8d5d9dbd45aa59f1e
Kevin Carufel <kevin.carufel@hotmail.com>
2023-01-22 16:46:42 -0500

    MAJ class BME280

--- src/AMOC-TP3-SOURCE/include/BME280.h
+++ src/AMOC-TP3-SOURCE/include/BME280.h
@@ -1,19 +1,18 @@
 #pragma once
-#include <Adafruit_BME280.h>
-#include <Adafruit_Sensor.h>
+
 #include <Arduino.h>
 
+class Adafruit_BME280;
+class Adafruit_Sensor;
 class BME280
 {
 private:
-    
-
 public:
     float m_temperature;
     float m_pression;
     float m_altitude;
     String m_message;
-    Adafruit_BME280 bme;
+    Adafruit_BME280 *m_bme;
     BME280();
     void tick();
 };
