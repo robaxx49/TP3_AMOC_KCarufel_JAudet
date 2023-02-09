e3af6741bef87a4bc27262c63e0ba20f7e295435
kev-42 <92613076+kev-42@users.noreply.github.com>
2023-02-09 12:22:24 -0500

    update

--- src/AMOC-TP3-SOURCE/include/configMQTT.h
+++ src/AMOC-TP3-SOURCE/include/configMQTT.h
@@ -4,7 +4,9 @@
 #define brokerPassword "admin"
 
 #define mqtt_server "192.168.23.123"
-#define mqtt_server_cegep "192.168.23.102"
+#define mqtt_server_cegep "192.168.23.199"
+#define mqtt_server_lab "192.168.23.122"
+#define mqtt_server_cell "192.168.43.162"
 
 #define brokerPort 1883
 #define brokerTopic "test/broker"
